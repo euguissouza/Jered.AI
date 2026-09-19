df6b69bb5349f1b799049ce9aac2fe9a182dd6bd
euguissouza <contato.guisilva4@hotmail.com>
2026-09-19 18:06:51 -0300

    first-commit

---
+++ jered.ai.api/src/test/java/com/api/jered/ai/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jered.ai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
