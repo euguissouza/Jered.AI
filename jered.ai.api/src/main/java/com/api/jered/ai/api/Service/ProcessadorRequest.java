8b558318834fec88a375c6fbcf99495ac0bd83a2
euguissouza <contato.guisilva4@hotmail.com>
2026-09-22 17:55:03 -0300

    Integração para processar os arquivos e devolver o download

---
+++ jered.ai.api/src/main/java/com/api/jered/ai/api/Service/ProcessadorRequest.java
@@ -0,0 +1,31 @@
+package com.api.jered.ai.api.Service;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ProcessadorRequest{
+
+    private AiEngine engine;
+
+    public ProcessadorRequest(AiEngine engine) {
+        this.engine = engine;
+    }
+
+    public ResponseEntity<Byte[]> DownloadFile(MultipartFile file) throws  Exception{
+        try{
+            Byte[] arquivos = engine.uploadArquivo(file);
+            String nomeOriginal = file.getOriginalFilename();
+            String nomeDownload = "Resumo" + nomeOriginal;
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + nomeDownload + "\"")
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM).body(arquivos);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+}
