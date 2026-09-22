eabe325032460885d30e81f8af8a2517e4332b5f
euguissouza <contato.guisilva4@hotmail.com>
2026-09-22 17:50:34 -0300

    Nova chamada de arquivo

--- jered.ai.api/src/main/java/com/api/jered/ai/api/Service/AiEngine.java
+++ jered.ai.api/src/main/java/com/api/jered/ai/api/Service/AiEngine.java
@@ -1,29 +1,45 @@
 package com.api.jered.ai.api.Service;
 
-import com.api.jered.ai.api.Model.Arquivos;
+
+import com.google.genai.Client;
+
+
+import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.model.googleai.GeminiFiles;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 @Service
-public class AiEngine extends Arquivos{
+public class AiEngine {
 
 
     private final GeminiFiles filesApi;
 
     public AiEngine(GeminiFiles filesApi) {
-        super();
         this.filesApi = filesApi;
     }
 
-    public void uploadArquivo() throws IOException, InterruptedException {
-        Path filesPath = Paths.get(getFiles().toUri());
-        GeminiFiles.GeminiFile uploadFile = filesApi.uploadFile(filesPath, getNome());
+    public Byte[] uploadArquivo(MultipartFile file) throws IOException, InterruptedException {
 
+        Path arquivoTemporario = Files.createTempFile("upload", "-" + file.getOriginalFilename());
+        try {
+            file.transferTo(arquivoTemporario.toFile());
+            GeminiFiles.GeminiFile uploadedFile = filesApi.uploadFile(
+                    arquivoTemporario,
+                    file.getOriginalFilename()
+            );
+        } finally {
+            Files.deleteIfExists(arquivoTemporario);
+        }
+        return uploadArquivo(file);
     }
-
 
 }
